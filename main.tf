66d01e8a21834b5e1ff2e4e4f8f49aa495297170
Mickleissa <mickleissa@gmail.com>
2021-10-13 22:38:50 -0400

    update the organization to mickleissa

--- main.tf
+++ main.tf
@@ -12,7 +12,7 @@ terraform {
   required_version = ">= 0.14"
 
   backend "remote" {
-    organization = "company-mickleissa"
+    organization = "mickleissa"
 
     workspaces {
       name = "gh-actions-demo"
